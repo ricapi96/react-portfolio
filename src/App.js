476f023b79f264862a32294266ffeff524d8de6d
Erica Roquemore <enroquemore@gmail.com>
2021-07-10 23:47:08 -0400

    added footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import About from "./components/About";
 import Work from "./components/Work";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <About />
       <Work />
+      <Footer />
     </body>
   );
 }
