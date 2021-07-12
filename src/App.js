c33c398c2a7eb93898f6d8a48760a1d1b308c861
Erica Roquemore <enroquemore@gmail.com>
2021-07-12 13:10:39 -0400

    fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
+import React, { Component } from 'react';
+import "./App.css";
+import Sidebar from './components/Sidebar';
 import Header from "./components/Header";
 import About from "./components/About";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import "./App.css";
 
-function App() {
-  return (
-    <body>
-      <Header />
-      <About />
-      <Work />
-      <Footer />
-    </body>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id= "colorlib page">
+        <div id= "container-wrap">
+          <Sidebar></Sidebar>
+          <div id= "colorlib-main">
+            <Header></Header>
+            <About></About>
+            <Work></Work>
+            <Contact></Contact>
+            <Footer></Footer>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
