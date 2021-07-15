6771e1f019c006bc800298a9e3d38d9e6e642334
Erica Roquemore <enroquemore@gmail.com>
2021-07-14 22:58:03 -0400

    added profile image

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import profile from "../Assets/images/profile.jpg";
 
 export default function About() {
-
-    return (
-        <div className="row">
-
-            <div id="about_me" className="col-md-2 col-sm-12">
-                About Me
-            </div>
-            <div id="middle" className="col-md-10 col-sm-12">
-                <p>Born and raised in Metro Atlanta, I was just a kid when I became interested in computers. However, after high school, I started working in restaurants, moving my way up from server to bartender, and then to management. I stayed in that field for 15 years. It taught me how to deal with people and develop my soft skills. It wasn't until the pandemic hit that I really started to consider a career in Web development. I was hospitalized in July 2020, and once I got home and recovered, I enrolled in a coding bootcamp at Georgia Tech. Also, I went back to school and am earning my BA in Computer Science. I've learned so much in these past six months, and can't wait to start my new career as a Full Stack Web Developer!<br></br></p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="row">
+      <img src={profile} />
+      <br />
+      <div id="about_me" className="col-md-2 col-sm-12">
+        About Me
+      </div>
+      <div id="middle" className="col-md-10 col-sm-12">
+        <p>
+          Born and raised in Metro Atlanta, I was just a kid when I became
+          interested in computers. However, after high school, I started working
+          in restaurants, moving my way up from server to bartender, and then to
+          management. I stayed in that field for 15 years. It taught me how to
+          deal with people and develop my soft skills. It wasn't until the
+          pandemic hit that I really started to consider a career in Web
+          development. I was hospitalized in July 2020, and once I got home and
+          recovered, I enrolled in a coding bootcamp at Georgia Tech. Also, I
+          went back to school and am earning my BA in Computer Science. I've
+          learned so much in these past six months, and can't wait to start my
+          new career as a Full Stack Web Developer!<br></br>
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,100 +1,103 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import habit from '../Assets/images/habit.png';
-import note from '../Assets/images/note-taker.png';
-import watch from '../Assets/images/watch-sensor.png';
-import readme from '../Assets/images/readme-gen.png';
-import '../Assets/css/work.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import habit from "../Assets/images/habit.png";
+import note from "../Assets/images/note-taker.png";
+import watch from "../Assets/images/watch-sensor.png";
+import readme from "../Assets/images/readme-gen.png";
+import "../Assets/css/work.css";
 
 export default function Work() {
-    return (
-        
-  <div className="row row-cols-1 row-cols-md-2 g-4">
-    <div className="col">
-      <div className="card bg-dark text-white">
-        <img src={watch} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">The Watch Sensor</h5>
-          <p className="card-text">
-            This dynamic Website application made with HTML, CSS, and JavaScript
-            will allow a user to search for a movie title and get a list of
-            movies to select from. Once they have selected a movie, they can
-            click on it to reveal what platforms that movie is streaming from.
-          </p>
-         
-          <a
-            href="https://ashak90.github.io/project-01/"
-            className="btn btn-secondary"
-            role="button"
-            >See it here</a
-          >
-        </div>
-      </div>
-    </div>
+  return (
+    <div className="row row-cols-1 row-cols-md-2 g-4">
+      <div id="work" className="col">
+        {" "}
+        Work
+        <div className="card bg-dark text-white">
+          <img src={watch} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">The Watch Sensor</h5>
+            <p className="card-text">
+              This dynamic Website application made with HTML, CSS, and
+              JavaScript will allow a user to search for a movie title and get a
+              list of movies to select from. Once they have selected a movie,
+              they can click on it to reveal what platforms that movie is
+              streaming from.
+            </p>
 
-    
-    <div className="col">
-      <div className="card bg-dark text-white">
-        <img src={habit} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Habit Tracker</h5>
-          <p className="card-text">
-            This is a Habit Tracker App that allows a user to track their
-            habits, either good or bad. Along with Node.js, it uses Express.js
-            to create a RESTFUL API, and Handlebars.js as the template engine.
-            The database uses MySQL and Sequelize ORM.
-          </p>
-          <a
-            href="https://habittracking-app.herokuapp.com/"
-            className="btn btn-secondary"
-            role="button"
-            >See it here</a
-          >
+            <a
+              href="https://ashak90.github.io/project-01/"
+              className="btn btn-secondary"
+              role="button"
+            >
+              See it here
+            </a>
+          </div>
         </div>
       </div>
-    </div>
 
-    
-    <div className="col">
-      <div className="card bg-dark text-white">
-        <img src={note} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Express Note Taker</h5>
-          <p className="card-text">
-            This is a note taking application used to write and save notes using
-            Express.js. It saves and retrieves note data from a JSON file.
-          </p>
-          <a
-            href="https://taking-notes1.herokuapp.com/"
-            className="btn btn-secondary"
-            role="button"
-            >See it here</a
-          >
+      <div className="col">
+        <div className="card bg-dark text-white">
+          <img src={habit} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Habit Tracker</h5>
+            <p className="card-text">
+              This is a Habit Tracker App that allows a user to track their
+              habits, either good or bad. Along with Node.js, it uses Express.js
+              to create a RESTFUL API, and Handlebars.js as the template engine.
+              The database uses MySQL and Sequelize ORM.
+            </p>
+            <a
+              href="https://habittracking-app.herokuapp.com/"
+              className="btn btn-secondary"
+              role="button"
+            >
+              See it here
+            </a>
+          </div>
         </div>
       </div>
-    </div>
 
-    
-    <div className="col">
-      <div className="card bg-dark text-white">
-        <img src={readme} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">README Generator</h5>
-          <p className="card-text">
-            This allows one to quickly and easily create a README file by using
-            a command-line application to generate one. This allows the project
-            creator to devote more time to working on the project.
-          </p>
-          <a
-            href="https://github.com/ricapi96/readme-generator"
-            className="btn btn-secondary"
-            role="button"
-            >See it here</a
-          >
+      <div className="col">
+        <div className="card bg-dark text-white">
+          <img src={note} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Express Note Taker</h5>
+            <p className="card-text">
+              This is a note taking application used to write and save notes
+              using Express.js. It saves and retrieves note data from a JSON
+              file.
+            </p>
+            <a
+              href="https://taking-notes1.herokuapp.com/"
+              className="btn btn-secondary"
+              role="button"
+            >
+              See it here
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="col">
+        <div className="card bg-dark text-white">
+          <img src={readme} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">README Generator</h5>
+            <p className="card-text">
+              This allows one to quickly and easily create a README file by
+              using a command-line application to generate one. This allows the
+              project creator to devote more time to working on the project.
+            </p>
+            <a
+              href="https://github.com/ricapi96/readme-generator"
+              className="btn btn-secondary"
+              role="button"
+            >
+              See it here
+            </a>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-    );
+  );
 }
-
